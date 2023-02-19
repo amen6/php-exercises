eeabde9c4db848df234e0572cdd5a71e319f2109
Amen <jamen.jamne30@gmail.com>
2023-02-19 12:29:58 +0200

    basics1 step3

---
+++ basics-1/step3.php
@@ -0,0 +1,9 @@
+<?php
+$name = "Amen";
+$email ="amen.nafeh6@gmail.com";
+echo "My name is $name and my email is $email.";
+$height = 2;
+$width = 1;
+$area = $height * $width;
+echo "The area is $area."
+?>
