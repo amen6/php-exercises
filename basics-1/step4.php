cf6d49541efe813985fdb6030b55ad4d73a6c069
Amen <jamen.jamne30@gmail.com>
2023-02-19 14:58:06 +0200

    basics1 step4

---
+++ basics-1/step4.php
@@ -0,0 +1,8 @@
+<?php 
+$name = "Amen";
+$position = "Web Developer";
+$github_url = "https://github.com/amen6";
+echo "Hello, I'm $name, I'm a $position please check my github link $github_url.";
+echo "Hello, I'm " .$name. ", I'm a " .$position. " please check my github link " .$github_url. ".";
+echo "Hello, I'm {$name}, I'm a {$position}. Please check my github link {$github_url}."
+?>
