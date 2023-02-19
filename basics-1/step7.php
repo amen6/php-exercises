ef1d703eee651ad5b0b7988d5e13c3a8213a8c48
Amen <jamen.jamne30@gmail.com>
2023-02-19 16:42:18 +0200

    basics1 step7

---
+++ basics-1/step7.php
@@ -0,0 +1,9 @@
+<?php 
+$birth_year = 2004;
+$current_year = date("Y");
+if ($current_year - $birth_year >= 18){
+    echo "You can drive";
+} else {
+    echo "You still a kid, go and play GTA";
+}
+?>
