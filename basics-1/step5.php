25ccede8ff09db240694f27508f17e0c6d09fc10
Amen <jamen.jamne30@gmail.com>
2023-02-19 16:08:35 +0200

    basics1 step5

---
+++ basics-1/step5.php
@@ -0,0 +1,10 @@
+<?php 
+$course_name = "Rust, the language of no bugs";
+$enrolled_students = 20;
+$price = 30;
+$on_discount = true;
+echo "Course title: $course_name <br>";
+echo "Enrolled Students: $enrolled_students <br>";
+echo "Course price: $price $ USD <br>";
+echo "Course on discount: " .($on_discount ? "Yes" : "No"). "<br>";
+?>
